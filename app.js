d419c4e55fce956b7492d3f89b64d32a74c87005
taofeichen@gmail.com <taofeichen@gmail.com>
2018-05-05 01:02:39 -0700

    app.js update

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-//added below
 var favicon = require('serve-favicon');
-var bodyParser = require('body-parser'); 
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 // Add for Database
 // New code that knows mongo and a nice abstraction layer
-var mongo = require('mongodb'); 
-var monk = require('monk'); 
+var mongo =require('mongodb');
+var monk =require('monk');
 // add a connection string to mlab.com
-// not using local db:  var db = monk('localhost:27017/nodetest2');
+// not using local db:  
+//var db = monk('localhost:27017/nodetest2');
 // use our connection string copied from the cloud mongo.
 var db = monk('mongodb://bc_taofei:bctaofei1226@ds014648.mlab.com:14648/prog219-chen');
 
-
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -27,35 +24,53 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Make our db accessible to our router 
-app.use(function(req,res,next){ 
-  req.db = db;
-  next();
-}); 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
