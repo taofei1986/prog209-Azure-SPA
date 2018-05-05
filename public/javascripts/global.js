186a6c0de0f3411d5bfa1012a6330ecbd7aa9af2
taofeichen@gmail.com <taofeichen@gmail.com>
2018-05-05 00:50:09 -0700

    change populateTable

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -19,29 +19,30 @@ $(document).ready(function() {
 
 // Functions ================================
 
-// Fill table with data
-function populateTable() { 
 
-    // Empty content string
-    var tableContent = '';
-  
-    // jQuery AJAX call for JSON 
-    $.getJSON( '/users/userlist', function( data ) { 
-        userListData = data; 
-      // For each item in our JSON, add a table row and cells to the content string
-      $.each(data, function(){
-        tableContent += '<tr>';
-        tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' 
-              + this.username + '</a></td>';
-        tableContent += '<td>' + this.email + '</td>';
-        tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-        tableContent += '</tr>';
-       }); 
-  
-      // Inject the whole content string into our existing HTML table
-      $('#userList table tbody').html(tableContent);
+// Fill table with data
+function populateTable() {
+
+  // Empty content string
+  var tableContent = '';
+
+  // jQuery AJAX call for JSON
+  $.getJSON( '/users/userlist', function( data ) {
+    userListData = data;
+   // For each item in our JSON, add a table row and cells to the content string
+    $.each(data, function(){
+      tableContent += '<tr>';
+      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+      tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+      tableContent += '</tr>';
     });
-  }; 
+
+    // Inject the whole content string into our existing HTML table
+    $('#userList table tbody').html(tableContent);
+  });
+};  // end of populateTable
+
 
 
 // Add User
